d985d2dbe0b7af444ccfe3f3c510bc6cb1dfadfc
stanislav <stanislavmuratov@yandex.ru>
2022-11-17 15:40:26 +0300

    Add Filter

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Final test
+// Input data
+string[] firstArray = { "hello", "2", "world", ":-)" };
+string[] secondArray = { "1234", "1567", "-2", "computer science" };
+string[] thirdArray = { "Russia", "Denmark", "Kazan" };
+
+string[] FilterStringForLength(string[] arr, int strLength)
+{
+    return Array.FindAll(arr, s => s.Length <= strLength);
+}
+
+void PrintMyArray(string[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+PrintMyArray(FilterStringForLength(secondArray, 3));
+
